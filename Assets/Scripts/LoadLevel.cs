d81412b34d47bc46fb7d0656ff1c780d53a5884c
JackBaker1 <163625@chrome.escg.ac.uk>
2022-03-28 20:23:22 +0100

    added scene transition

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelToLoad : MonoBehaviour
+{
+    public string levelToLoad;
+
+    public string exitPoint;
+
+    private  PM thePlayer;
+
+    public string Test { get; private set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = FindObjectOfType<PM>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            Application.LoadLevel(levelToLoad);
+            thePlayer.startPoint = exitPoint;
+
+          
+        }
+    }
+}
