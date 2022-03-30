f989a2365678de6f959cc41da578dcaf6c590e94
JackBaker1 <163625@chrome.escg.ac.uk>
2022-03-30 16:15:19 +0100

    Finished level 1, added parallax background and started level 2

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelToLoad : MonoBehaviour
+public class LoadLevel : MonoBehaviour
 {
     public string levelToLoad;
 
